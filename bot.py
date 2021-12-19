ff81d15e3ed86b266600b6c72b9bd34bdba92193
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-12-19 11:52:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,14 +14,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, 
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
 
-Cat = ["https://telegra.ph/file/e251e55ce213b2e9f4c03.jpg",
-           "https://telegra.ph/file/47dba9369c1660fd7bf87.jpg",
-           "https://telegra.ph/file/91da14cb4c250e833206a.jpg",
-           "https://telegra.ph/file/2964d8516056676e82099.jpg",
-           "https://telegra.ph/file/8cc2965db0ea46ea1318f.jpg",
-           "https://telegra.ph/file/b886ff9934e6519278a53.jpg",
-           "https://telegra.ph/file/6d37256aace9919242e46.jpg"]
-
 
 Bot = Client(
     "Song Downloader Bot",
@@ -66,7 +58,6 @@ ABOUT_TEXT = """
 ║┣⪼ Sᴏᴜʀᴄᴇ -  [Cʟɪᴄᴋ Hᴇʀᴇ](https://t.me/NOKIERUNNOIPPKITTUM)
 ║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪
 """
-await cmd.reply_photo(photo = random.choice(Cat),
 START_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ', url=f"https://telegram.me/{Config.SUPPORT}"), 
