bed411230f2e3d3f3b897a26cee585e0af5f159a
Psycho_hacker <69103277+hopepsycho@users.noreply.github.com>
2022-04-04 13:39:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ Bot = Client(
 db = Database()
 START_TEXT = """Hᴀɪ {}, 
 Aᴍ ᴀ YᴏᴜTᴜʙᴇ Dᴏᴡɴʟᴏᴀᴅᴇʀ Bᴏᴛ I Cᴀɴ Dᴏᴡɴʟᴏᴀᴅ Sᴏɴɢs,Vɪᴅᴇᴏs & Lʏʀɪᴄs Fʀᴏᴍ YᴏᴜTᴜʙᴇ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ɪɴᴛᴏ Tᴇʟᴇɢʀᴀᴍ.\n
-Usᴇ /help Cᴏᴍᴍᴀɴᴅs Fᴏʀ Mᴏʀᴇ.\n\nBʏ <a href="https://t.me/VAMPIRE_KING_NO_1">Tᴇʀʀᴏʀ Mɪᴄᴋᴇʏ</a>
+Usᴇ /help Cᴏᴍᴍᴀɴᴅs Fᴏʀ Mᴏʀᴇ.\n\nBʏ <a href="https://t.me/king_of_psycho">Tᴇʀʀᴏʀ Mɪᴄᴋᴇʏ</a>
 """
 
 CMDS_TEXT = """
@@ -49,13 +49,13 @@ CMDS_TEXT = """
 ABOUT_TEXT = """
 ╔════❰ Aʙᴏᴜᴛ ❱═❍⊱❁۪۪
 ║╭━━━━━━━━━━━━━━━➣ 
-║┣⪼ Mʏ Nᴀᴍᴇ - <a href="http://t.me/BlackCatMSRobot">Bʟᴀᴄᴋ Cᴀᴛ</a>
-║┣⪼ Cʀᴇᴀᴛᴏʀ - <a href=https://t.me/VAMPIRE_KING_NO_1>ƬЄƦƦƠƦ MƖƇƘЄƳ</a>
+║┣⪼ Mʏ Nᴀᴍᴇ - <a href="http://t.me/ANGELOFDEVILBOT">Aɴɢᴇʟ Sᴏɴɢ</a>
+║┣⪼ Cʀᴇᴀᴛᴏʀ - <a href=https://t.me/king_of_psycho>ƬЄƦƦƠƦ MƖƇƘЄƳ</a>
 ║┣⪼ Lɪʙʀᴀʀʏ - <a href="https://docs.pyrogram.org/"> Pʏʀᴏɢʀᴀᴍ </a>
 ║┣⪼ Lᴀɴɢᴜᴀɢᴇ - Pʏᴛʜᴏɴ 3 
-║┣⪼ Sᴜᴘᴘᴏʀᴛ - <a href="https://t.me/STMbOTsUPPORTgROUP">Mᴇᴘʜɪsᴛᴏ Bᴏᴛ Sᴜᴘᴘᴏʀᴛ</a>
+║┣⪼ Sᴜᴘᴘᴏʀᴛ - <a href="https://t.me/kunjappansupportez">Mᴇᴘʜɪsᴛᴏ Bᴏᴛ Sᴜᴘᴘᴏʀᴛ</a>
 ║┣⪼ ⲂⲞⲦ Sᴇʀᴠᴇʀ - Hᴇʀᴏᴋᴜ
-║┣⪼ Sᴏᴜʀᴄᴇ -  [Cʟɪᴄᴋ Hᴇʀᴇ](https://t.me/NOKIERUNNOIPPKITTUM)
+║┣⪼ Sᴏᴜʀᴄᴇ -  [Cʟɪᴄᴋ Hᴇʀᴇ](https://GitHub.com/hopepsycho)
 ║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪
 """
 START_BUTTONS = InlineKeyboardMarkup(
